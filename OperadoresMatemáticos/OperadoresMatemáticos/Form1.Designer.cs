7e3d18abeb1dd308d984a6e2174751d673029474
igorlaur <39922652+igorlaur@users.noreply.github.com>
2018-09-30 22:29:27 -0300

    Projeto C#

---
+++ OperadoresMatemáticos/OperadoresMatemáticos/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace OperadoresMatemáticos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmd_executar = new System.Windows.Forms.Button();
+            this.label_resultado = new System.Windows.Forms.Label();
+            this.text_parcela_2 = new System.Windows.Forms.TextBox();
+            this.text_parcela_1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmd_executar
+            // 
+            this.cmd_executar.Location = new System.Drawing.Point(12, 144);
+            this.cmd_executar.Name = "cmd_executar";
+            this.cmd_executar.Size = new System.Drawing.Size(126, 44);
+            this.cmd_executar.TabIndex = 0;
+            this.cmd_executar.Text = "Executar";
+            this.cmd_executar.UseVisualStyleBackColor = true;
+            this.cmd_executar.Click += new System.EventHandler(this.cmd_executar_Click);
+            // 
+            // label_resultado
+            // 
+            this.label_resultado.AutoSize = true;
+            this.label_resultado.Location = new System.Drawing.Point(12, 110);
+            this.label_resultado.Name = "label_resultado";
+            this.label_resultado.Size = new System.Drawing.Size(0, 17);
+            this.label_resultado.TabIndex = 1;
+            // 
+            // text_parcela_2
+            // 
+            this.text_parcela_2.Location = new System.Drawing.Point(15, 53);
+            this.text_parcela_2.Name = "text_parcela_2";
+            this.text_parcela_2.Size = new System.Drawing.Size(123, 22);
+            this.text_parcela_2.TabIndex = 2;
+            // 
+            // text_parcela_1
+            // 
+            this.text_parcela_1.Location = new System.Drawing.Point(15, 25);
+            this.text_parcela_1.Name = "text_parcela_1";
+            this.text_parcela_1.Size = new System.Drawing.Size(123, 22);
+            this.text_parcela_1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.text_parcela_1);
+            this.Controls.Add(this.text_parcela_2);
+            this.Controls.Add(this.label_resultado);
+            this.Controls.Add(this.cmd_executar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmd_executar;
+        private System.Windows.Forms.Label label_resultado;
+        private System.Windows.Forms.TextBox text_parcela_2;
+        private System.Windows.Forms.TextBox text_parcela_1;
+    }
+}
+
---
+++ OperadoresMatemáticos/OperadoresMatemáticos/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OperadoresMatemáticos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmd_executar_Click(object sender, EventArgs e)
+        {
+           
+            int parcela_1 = int.Parse(text_parcela_1.Text);
+            int parcela_2 = int.Parse(text_parcela_2.Text);
+            //int resultado;
+
+            /// Adição (+)
+
+            //resultado = parcela_1 + parcela_2;
+
+            /// Subtração (-)
+
+            //resultado = parcela_1 - parcela_2;
+
+            /// Multiplicação (*)
+
+            //resultado = parcela_1 * parcela_2;
+
+            /// Divisão (/)
+
+            //resultado = parcela_1 / parcela_2;
+
+            /// Módulos (Resto da divisão) (%)
+
+            //resultado = parcela_1 % parcela_2;
+
+            int resultado = 50;
+
+            //resultado = resultado + 1;
+            //resultado++;      // incremento automático
+            //resultado += 5    /* resultado = resultado + 5 */
+
+            label_resultado.Text = (++resultado).ToString();// Converter resultado numérico para alfanumérico
+
+        }
+    }
+}
