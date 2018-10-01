61917c0f7cc4017f1db2fab59543e35488b04b22
igorlaur <39922652+igorlaur@users.noreply.github.com>
2018-09-30 22:00:44 -0300

    Projeto C#

--- PrimeiraAplicacao/PrimeiraAplicacao/Form1.Designer.cs
+++ PrimeiraAplicacao/PrimeiraAplicacao/Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.btn_executar = new System.Windows.Forms.Button();
             this.caixa_texto = new System.Windows.Forms.Label();
+            this.btn_fechar = new System.Windows.Forms.Button();
+            this.caixa_texto2 = new System.Windows.Forms.Label();
+            this.btn_clique = new System.Windows.Forms.Button();
+            this.caixa_clique = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_executar
@@ -46,9 +51,56 @@
             // 
             this.caixa_texto.Location = new System.Drawing.Point(12, 109);
             this.caixa_texto.Name = "caixa_texto";
-            this.caixa_texto.Size = new System.Drawing.Size(794, 80);
+            this.caixa_texto.Size = new System.Drawing.Size(171, 80);
             this.caixa_texto.TabIndex = 1;
             this.caixa_texto.Text = "Clique em executar";
+            // 
+            // btn_fechar
+            // 
+            this.btn_fechar.Location = new System.Drawing.Point(455, 389);
+            this.btn_fechar.Name = "btn_fechar";
+            this.btn_fechar.Size = new System.Drawing.Size(154, 55);
+            this.btn_fechar.TabIndex = 2;
+            this.btn_fechar.Text = "Fechar";
+            this.btn_fechar.UseVisualStyleBackColor = true;
+            this.btn_fechar.Click += new System.EventHandler(this.btn_prosseguir_Click);
+            // 
+            // caixa_texto2
+            // 
+            this.caixa_texto2.Location = new System.Drawing.Point(12, 189);
+            this.caixa_texto2.Name = "caixa_texto2";
+            this.caixa_texto2.Size = new System.Drawing.Size(153, 80);
+            this.caixa_texto2.TabIndex = 4;
+            this.caixa_texto2.Text = "Clique em Fechar";
+            this.caixa_texto2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btn_clique
+            // 
+            this.btn_clique.Location = new System.Drawing.Point(101, 389);
+            this.btn_clique.Name = "btn_clique";
+            this.btn_clique.Size = new System.Drawing.Size(154, 55);
+            this.btn_clique.TabIndex = 5;
+            this.btn_clique.Text = "Clique aqui";
+            this.btn_clique.UseVisualStyleBackColor = true;
+            this.btn_clique.Click += new System.EventHandler(this.btn_clique_Click);
+            // 
+            // caixa_clique
+            // 
+            this.caixa_clique.Location = new System.Drawing.Point(12, 269);
+            this.caixa_clique.Name = "caixa_clique";
+            this.caixa_clique.Size = new System.Drawing.Size(153, 80);
+            this.caixa_clique.TabIndex = 6;
+            this.caixa_clique.Text = "Clique em Fechar";
+            this.caixa_clique.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(475, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
@@ -56,11 +108,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(818, 456);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.caixa_clique);
+            this.Controls.Add(this.btn_clique);
+            this.Controls.Add(this.caixa_texto2);
+            this.Controls.Add(this.btn_fechar);
             this.Controls.Add(this.caixa_texto);
             this.Controls.Add(this.btn_executar);
             this.Name = "Form1";
             this.Text = "Laur";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +126,11 @@
 
         private System.Windows.Forms.Button btn_executar;
         private System.Windows.Forms.Label caixa_texto;
+        private System.Windows.Forms.Button btn_fechar;
+        private System.Windows.Forms.Label caixa_texto2;
+        private System.Windows.Forms.Button btn_clique;
+        private System.Windows.Forms.Label caixa_clique;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- PrimeiraAplicacao/PrimeiraAplicacao/Form1.cs
+++ PrimeiraAplicacao/PrimeiraAplicacao/Form1.cs
@@ -19,7 +19,74 @@ namespace PrimeiraAplicacao
 
         private void btn_executar_Click(object sender, EventArgs e)
         {
-            caixa_texto.Text = "Olá, Igor Laur!";
+            /* Region serve para eu esconder meu código */
+            #region região com nome alterado
+
+            // Numéricas
+
+            //byte = 0 to 255
+            byte idade = 45;
+
+            //short = -32,768 to 32,767
+            short idade2 = 45;
+
+            //int = -2.147.483.648 to -2.147.483.648
+            int idade_planeta = 234982347;
+
+            //long = -9.223.372.036.854.775.808 to -9.223.372.036.854.775.808
+            long valor_enorme = 3245683746354;
+
+
+            //COM CASAS DECIMAIS
+
+            float valor1 = 10.5f;
+            double valor2 = 20.3;
+            decimal valor3 = 30.7m;
+
+            caixa_texto.Text = valor3.ToString();
+
+            /*string resultado = "Igor Laur";
+            int numero_caracteres = resultado.Length;
+            caixa_texto.Text = "Nº caracteres = " + numero_caracteres;*/
+
+            // ALFANUMERICAS 
+            /*
+            string resultado = "Igor Laur";
+            char caracter = '1';
+            caixa_texto.Text = caracter.ToString();
+            */
+            #endregion
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_prosseguir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_clique_Click(object sender, EventArgs e)
+        {
+            // criar variável - tipo
+            #region Região 
+            string nome = "Igor Laur";
+            int idade = 20;
+            int segunda_idade = 22;
+
+            int idade_total = idade + segunda_idade;
+
+            caixa_clique.Text = "Me chamo " + nome + " e tenho " + idade.ToString() + " anos. Meu amigo tem " +
+                segunda_idade + " a somatória de nossas idades = " + idade_total;
+            #endregion
+
         }
     }
 }
